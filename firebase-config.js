1711d93a6633a7d10ce6d83c9816f1892deeb705
guu42 <126980196+guu42@users.noreply.github.com>
2026-08-18 00:13:10 +0200

    Update Firebase config by removing measurementId
    Removed measurementId from Firebase configuration.

--- firebase-config.js
+++ firebase-config.js
@@ -8,6 +8,5 @@ export const firebaseConfig = {
   projectId: "irl-achievement-app",
   storageBucket: "irl-achievement-app.firebasestorage.app",
   messagingSenderId: "909484054114",
-  appId: "1:909484054114:web:741d879fc15ba30b01633b",
-  measurementId: "G-K6B0YEDNEC",
+  appId: "1:909484054114:web:741d879fc15ba30b01633b"
 };
